3fc0c94178635f95a27699cf3325a2839e96fade
harshani <ruwanthikapemarathna@gmail.com>
2024-04-28 08:50:20 +0530

    Added authMiddleware

---
+++ middleware/AuthMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.SECRET_KEY;
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(403).json({ 'error': 'token is missing!' });
+    }
+    jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ 'error': 'token is invalid!' });
+        }
+
+        next();
+    });
+}
+module.exports = verifyToken;
